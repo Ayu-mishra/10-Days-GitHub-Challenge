af9cef37cfdc75905060f467beef91a1b6e2374b
Anurag Mishra <anuragayumishra@gmail.com>
2021-01-20 22:59:16 +0530

    Check Armstrong Number

---
+++ armstrong_no.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int i=0,d,sum=0,num;
+	printf("enter a number");
+	scanf("%d",&num);
+	d=num;
+	while(num!=0)
+	{
+		i=num%10;
+		num=num/10;
+		sum=sum+i*i*i;
+		
+	}
+if(d==sum)
+printf("no is armstrong");
+else
+printf("no is not armstrong");
+
+}
