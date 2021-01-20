62ab262a8b1499fbb7ba4a5566fb212c622bdf65
Anurag Mishra <anuragayumishra@gmail.com>
2021-01-20 22:57:08 +0530

    Find Greatest of the three values

---
+++ grearest in three variable.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{int a,b,c;
+printf("enter a,b and c");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
+{
+printf("a is greatest");}
+
+else if(b>a && b>c)
+{
+printf("b is greatest");}
+else
+{
+printf("c is gretest");}
+
+}
