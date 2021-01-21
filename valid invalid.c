a441c4c72077c8350e1eaaf4ceac6accebc8ccb4
Anurag Mishra <anuragayumishra@gmail.com>
2021-01-21 23:33:56 +0530

    Limit validator

---
+++ valid invalid.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+
+{
+	int a;
+	printf("enter a value");
+	scanf("%d",&a);
+	if( a>0 && a<=100)
+	printf("valid answer");
+	else 
+	printf("invalid answer");
+	
+	
+	
+	
+	
+}
