159856712241dea34cb3c961ffd21212fc025581
Anurag Mishra <anuragayumishra@gmail.com>
2021-01-21 23:38:35 +0530

    Odd number increment

---
+++ increasin odd term by 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{int a[10],i,j;
+cout<<"enter 10 no" ;
+for 
+(i=0;i<10;i++)
+cin>>a[i];
+for(i=0;i<10;i=i+1)
+{if (i%2!=0)
+a[i]=a[i]+2;}
+for(i=0;i<10;i++)
+
+{cout<< a[i];}
+return 0;
+}
