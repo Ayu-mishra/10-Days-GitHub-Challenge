9f07f3efdbe48b9298a7bfe6418f44de3d1bff04
Anurag Mishra <anuragayumishra@gmail.com>
2021-01-21 23:30:15 +0530

    Number of digits counter

---
+++ count of digits.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a,n,n1;
+	printf("enter the number");
+	scanf("%d",&n);
+	
+	n1=n;
+	
+	while(n!=0)
+	{
+		n=n/10; a=a+1;
+	}
+
+printf(" number of digits in  %d are %d",n1,a);
+	
+	}
