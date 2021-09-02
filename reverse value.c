dbe49f1f8c083834c74c0e5a0b7c728676b2141f
Anurag Mishra <anuragayumishra@gmail.com>
2021-09-02 23:50:27 +0530

    Update reverse value.c

--- reverse value.c
+++ reverse value.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
-{int n,s=0,m;
-printf("enter a no.");
-scanf("%d",&n);
-while(n!=0)
-{m=n%10;
-s=s*10+m;
-n=n/10;
-}
-printf("reversed value is%d",s);
+{ int n,s=0,m;
+  printf("enter a no.");
+  scanf("%d",&n);
+  while(n!=0)
+  { m=n%10;
+    s=s*10+m;
+    n=n/10;
+  }
+ printf("reversed value is%d",s);
 }
