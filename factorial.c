81668583fc1698d80b78730b05dc4a3216af221e
Anurag Mishra <anuragayumishra@gmail.com>
2021-12-20 00:04:33 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 void main()
-{int a,n,f;
-printf("enter a no.");
-scanf("%d",&n);
-for(a=1;a<=n;a=a+1)
-{f=f*a;}
-printf("factorial is %d",f);	                                                
+{
+  int a,n,f;
+  printf("enter a no.");
+  scanf("%d",&n);
+  for(a=1;a<=n;a=a+1)
+  {f=f*a;}
+  printf("factorial is %d",f);	                                                
 } 
